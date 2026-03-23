d4eeb22829323fa6556abcc0de8388daa77b0e04
Daniil Iakovlev <ydaniil29@gmail.com>
2026-03-23 08:33:18 +0300

    fix

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -4,7 +4,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/rspack';
 
 export default defineConfig({
   output: {
-    assetPrefix: '/industrial-legacy/',
+    assetPrefix: './',
   },
   plugins: [pluginReact()],
   tools: {
